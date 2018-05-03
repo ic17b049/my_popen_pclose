fc9ec3294fb387510520dc398c12dbb282d602e3
Dominic Schebeck <ic17b049@technikum-wien.at>
2018-05-03 22:58:44 +0200

    03.05. v2

--- mypopen.c
+++ mypopen.c
@@ -17,53 +17,14 @@
 * ------------------------------------------------------includes----------
 */
 
+#include "mypopen.h"
 #include <unistd.h>
 #include <stdio.h>
-#include "mypopen.h"
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
-
-
-/* TEST STATUS
-./popentest: Test "mypopentest00" passed
-./popentest: Test "mypopentest01" passed
-./popentest: Test "mypopentest02" passed
-./popentest: Test "mypopentest03" passed
-./popentest: Test "mypopentest04" passed
-./popentest: Test "mypopentest05" passed
-./popentest: Test "mypopentest06" passed
-./popentest: Test "mypopentest07" passed
-./popentest: Test "mypopentest08" passed
-./popentest: Test "mypopentest09" passed
-./popentest: Test "mypopentest10" passed
-./popentest: Test "mypopentest11" passed
-./popentest: Test "mypopentest12" passed
-./popentest: Test "mypopentest13" passed
-./popentest: Test "mypopentest14" passed
-./popentest: Test "mypopentest15" passed
-./popentest: Test "mypopentest16" passed
-./popentest: Test "mypopentest17" passed
-./popentest: Test "mypopentest18" passed
-./popentest: Test "mypopentest19" passed
-./popentest: Test "mypopentest20" passed
-./popentest: Test "mypopentest21" passed
-./popentest: Test "mypopentest22" passed
-./popentest: Test "mypopentest23" passed
-./popentest: Test "mypopentest24" failed at line 2354 - This is not tolerated
-./popentest: Test "mypopentest25" failed at line 2466 - This is not tolerated
-./popentest: Test "mypopentest26" passed
-./popentest: Test "mypopentest27" failed at line 2492 - This is not tolerated
-./popentest: Test "mypopentest27" passed
-./popentest: Test "mypopentest28" failed at line 2265 - This is not tolerated
-./popentest: Test "mypopentest28" passed
-./popentest: Test "mypopentest29" passed
-./popentest: Test "mypopentest30" passed
-*/
-
-
 
 static pid_t pid = -1;
 FILE *retpointer = NULL;
@@ -78,11 +39,15 @@ FILE *mypopen(const char *command, const char *type){
 	}
 	
 	
-	if((type[0] != 'r' && type[0] != 'w') || type[1] != '\0'){
+	if(type[0] != 'r' && type[0] != 'w'){
 		errno = EINVAL;
 		return NULL;
 	}	
 	
+	if(type[1] != '\0'){
+		errno = EINVAL;
+		return NULL;
+	}
 	
 	if(pid != -1){
 		errno = EAGAIN;
@@ -93,76 +58,80 @@ FILE *mypopen(const char *command, const char *type){
 	int pipefd[2];
 
 	if (pipe(pipefd) == -1) {
-		// erno already set by pipe
+		// errno already set by pipe
 		return NULL;
 	}
 
     pid = fork();
-	
-	if(pid == -1){
-		close(pipefd[PIPE_FD_READ]);
-        close(pipefd[PIPE_FD_WRITE]);
-		errno = EAGAIN;
-		return NULL;
-	}
-		
-	if(pid == 0){
-		//child		
-			
-		if(type[0] == 'r')
-		{
+
+	switch(pid) {
+		case -1:
 			close(pipefd[PIPE_FD_READ]);
-			//mypopentest24/25
-			if(pipefd[PIPE_FD_WRITE] != STDOUT_FILENO)
-			{
-				if(dup2(pipefd[PIPE_FD_WRITE], STDOUT_FILENO) == -1)
-				{
-					close(pipefd[PIPE_FD_WRITE]);
-					_Exit(EXIT_FAILURE);
-				}
-				close(pipefd[PIPE_FD_WRITE]);
-			}
-		}
-		else 
-		{
 			close(pipefd[PIPE_FD_WRITE]);
-			//mypopentest24/25
-			if(pipefd[PIPE_FD_READ] != STDIN_FILENO)
+			errno = EAGAIN;
+			return NULL;			
+		break;
+		
+		case 0:
+			//child		
+			if(type[0] == 'r')
 			{
-				if(dup2(pipefd[PIPE_FD_READ], STDIN_FILENO) == -1)
-				{
-					close(pipefd[PIPE_FD_READ]);
-					_Exit(EXIT_FAILURE);
-				}
 				close(pipefd[PIPE_FD_READ]);
+				if(pipefd[PIPE_FD_WRITE] != STDOUT_FILENO)
+				{
+					if(dup2(pipefd[PIPE_FD_WRITE], STDOUT_FILENO) == -1)
+					{
+						close(pipefd[PIPE_FD_WRITE]);
+						_Exit(EXIT_FAILURE);
+					}
+					close(pipefd[PIPE_FD_WRITE]);
+				}
 			}
-		}			
 			
-			
-			
-		execl("/bin/sh", "sh", "-c", command, (char*) NULL);
-		//reaches only when execl fails
-		_Exit(EXIT_FAILURE);
-		
-	}else{
-		if(type[0] == 'r') {
-			close(pipefd[PIPE_FD_WRITE]);
-			retpointer =  fdopen(pipefd[PIPE_FD_READ], "r");
-		}
-		if(type[0] == 'w'){
-			close(pipefd[PIPE_FD_READ]);
-			retpointer =  fdopen(pipefd[PIPE_FD_WRITE], "w");
-		}
+			if(type[0] == 'w')
+			{
+				close(pipefd[PIPE_FD_WRITE]);
+				if(pipefd[PIPE_FD_READ] != STDIN_FILENO)
+				{
+					if(dup2(pipefd[PIPE_FD_READ], STDIN_FILENO) == -1)
+					{
+						close(pipefd[PIPE_FD_READ]);
+						_Exit(EXIT_FAILURE);
+					}
+					close(pipefd[PIPE_FD_READ]);
+				}
+			}			
+	
+			execl("/bin/sh", "sh", "-c", command, (char*) NULL);
+			//reaches only when execl fails
+			_Exit(EXIT_FAILURE);
+		break;
+
+		default:
+			//parent
+			if(type[0] == 'r') {
+				close(pipefd[PIPE_FD_WRITE]);
+				retpointer =  fdopen(pipefd[PIPE_FD_READ], "r");
+			}
+			if(type[0] == 'w'){
+				close(pipefd[PIPE_FD_READ]);
+				retpointer =  fdopen(pipefd[PIPE_FD_WRITE], "w");
+			}
+		break;
 	}
 
 	if(retpointer == NULL){
+
+		//reset Vars.
 		pid = -1;
-		retpointer = NULL;
+		//retpointer = NULL; is condition to reach
 		
-		if(type[0] == 'r') close(pipefd[PIPE_FD_READ]);
-		else if(type[0] == 'w') close(pipefd[PIPE_FD_WRITE]);
-	
-		else return NULL;
+		if(type[0] == 'r'){
+			close(pipefd[PIPE_FD_READ]);
+		}
+		if(type[0] == 'w'){
+			close(pipefd[PIPE_FD_WRITE]);
+		}
 	}
 	return retpointer;
 	 
@@ -181,34 +150,35 @@ int mypclose(FILE *stream){
 	}
 	
 	if(fclose(retpointer) == EOF){
+		
 		//reset vars.
 		pid = -1;
 		retpointer = NULL;
-		
+
 		return -1;
 	}
 	
-	int status = -1;
+	int status;
 	int wait_pid;
+	
 	do{
 		wait_pid = waitpid(pid, &status,0);
 	}while(wait_pid == -1 && errno == EINTR);
-    
-	
-	
-	
-	if(!(WIFEXITED(status))){
-		errno = ECHILD;
-		return -1;
-	}
-	if(wait_pid == -1){
-		return -1;
-	}
-	
-	//reset Vars.	
+
+	//reset Vars.
 	pid = -1;
 	retpointer = NULL;
+	
+    if (wait_pid == -1){
+        errno = ECHILD;
+        return -1;
+    }
 
-	return WEXITSTATUS(status);	
+    if (WIFEXITED(status)) {
+       return WEXITSTATUS(status);
+	}else{
+		errno = ECHILD;
+		return -1;		   
+	}
 }
 //------------------------------------eof--------------------------------------------------
